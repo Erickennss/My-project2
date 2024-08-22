149116cb841644839eeec7dd45bea76a1cd3aa9a
Erickennss <sodium645@gmail.com>
2024-08-22 22:28:03 +0800

    new commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,21 @@
 module.exports = {
+  // 设置为根配置文件
   root: true,
+  // 指定解析器
   parser: "@typescript-eslint/parser",
+  // 定义插件
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  // 扩展规则集
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:eslint-comments/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:node/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+  ],
   env: {
     node: true,
     es6: true,
