9cab676d5088888ce1f24f26f5d66eabfb9bf060
Erickennss <sodium645@gmail.com>
2024-08-23 12:37:49 +0800

    Github Action

--- dist/tsc/browser.js
+++ dist/tsc/browser.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // Import a function
 import { greet } from "./main";
-// const main_1 = require("./main");
 // Make it accessible on the window object
 window.greet = greet;
 //# sourceMappingURL=browser.js.map
