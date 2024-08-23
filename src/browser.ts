25b440c863a089c34fe5c17c5b8004ccda4e0f11
Erickennss <sodium645@gmail.com>
2024-08-23 12:33:47 +0800

    Github Action

--- src/browser.ts
+++ src/browser.ts
@@ -1,11 +1,17 @@
 // Import a function
-import { greet } from "./main";
-
-//  Make it accessible on the window object
 // (window as any).greet = greet;
+// Make it accessible on the window object
 
-try {
-  (window as unknown as { greet: typeof greet }).greet = greet;
-} catch (error) {
-  console.error("Error loading or assigning greet function:", error);
+declare global {
+  interface Window {
+    greet: typeof greet;
+  }
 }
+// In your browser.ts file
+import { greet } from "./main";
+window.greet = greet;
+// try {
+//   (window as unknown as { greet: typeof greet }).greet = greet;
+// } catch (error) {
+//   console.error("Error loading or assigning greet function:", error);
+// }
